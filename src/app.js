72c16d84069d3bbb282fcdca2b67eee3f9987d88
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-20 15:23:56 +0700

    Update cors

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const port = process.env.PORT || 8080;
 const app = express();
 const route = require("./routes/index");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const handleError = require("./common/error");
 require("./database/mongoose");
 
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 app.use("/", route);
 
 app.use((err, req, res, next) => {
