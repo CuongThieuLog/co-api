24319c0d3ed48bee4ae59c199a6dcc1124ec5510
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-20 21:31:18 +0700

    update

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,17 +2,26 @@ const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  const data = jwt.verify(token, process.env.JWT_KEY);
-
   try {
-    const user = await User.findOne({ _id: data._id, "tokens.token": token });
+    const token = req.header("Authorization");
+    if (!token || !token.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .send({ error: "Authorization token is missing or invalid" });
+    }
+
+    const tokenValue = token.replace("Bearer ", "");
+    const data = jwt.verify(tokenValue, process.env.JWT_KEY);
+    const user = await User.findOne({
+      _id: data._id,
+      "tokens.token": tokenValue,
+    });
 
     if (!user) {
       throw new Error();
     }
     req.user = user;
-    req.token = token;
+    req.token = tokenValue;
     next();
   } catch (error) {
     res.status(401).send({ error: "Not authorized to access this resource" });
